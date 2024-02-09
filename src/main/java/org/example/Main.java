83d73c91074b8f5e479954a312e83f728a409c57
Ermek Orazbekov <154459511+surf100@users.noreply.github.com>
2024-02-09 19:56:41 +0600

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,32 +10,30 @@ public abstract class Main {
         ArrayList<User> users = new ArrayList<>();
         ArrayList<Book> books = new ArrayList<>();
         Connection conn = null;
-        try{
+        try {
             Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection(connectionString,"postgres","keklolloh123");
+            conn = DriverManager.getConnection(connectionString, "postgres", "keklolloh123");
 
             String sql = "SELECT id, name, surname, status, email, password FROM public.\"user\" ORDER BY id";
             Statement stat = conn.createStatement();
 
             ResultSet rs = stat.executeQuery(sql);
-            while (rs.next()){
-                int    id = rs.getInt("id");
+            while (rs.next()) {
+                int id = rs.getInt("id");
                 String name = rs.getString("name");
                 String surname = rs.getString("surname");
                 boolean status = rs.getBoolean("status");
                 String email = rs.getString("email");
                 String password = rs.getString("password");
 
-                User user = new User(id,name,surname,status,email,password);
+                User user = new User(id, name, surname, status, email, password);
                 users.add(user);
             }
 
 
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }finally {
+        } finally {
             if (conn != null) {
                 try {
                     conn.close();
@@ -46,21 +44,33 @@ public abstract class Main {
         }
 
 
+
+
+
+
+
+
         try {
-            User user = new User(1,"1","1",true,"1","1");
-            Book book = new Book(1, "1", 1, "1");
+
+            User user = new User(1, "1", "1", true, "1", "1");
+            Book book = new Book(1, "1", 1, "1", 1);
             Scanner sc = new Scanner(System.in);
-            System.out.println("Do u want to Log in(l) or Sign up?(s):");
-            String choice = sc.nextLine();
-            if(choice.equals("s")){
+
+            System.out.println("0.Log in\n" + "1.Sign up");
+            int choice = sc.nextInt();
+
+            if (choice == 1) {
+
                 user.addElement();
                 conn.close();
+
                 conn = DriverManager.getConnection(connectionString, "postgres", "keklolloh123");
                 String sql = "SELECT id, name, surname, status, email, password FROM public.\"user\" ORDER BY id";
                 Statement stat = conn.createStatement();
                 ResultSet rs = stat.executeQuery(sql);
                 users.clear();
-                while (rs.next()){
+
+                while (rs.next()) {
                     int id = rs.getInt("id");
                     String name = rs.getString("name");
                     String surname = rs.getString("surname");
@@ -71,179 +81,126 @@ public abstract class Main {
                     User newUser = new User(id, name, surname, status, email, password);
                     users.add(newUser);
                 }
-                boolean userStatus = users.get(users.size()-1).isStatus();
-                try{
+
+                boolean userStatus = users.get(users.size() - 1).isStatus();
+
+                try {
                     if (userStatus) {
-                        System.out.println("Write number of the book that u want to take: ");
-                        int number = sc.nextInt();
-                        book.takeBook(number);
-                    }else{
-                        System.out.println("Add charachteristics of book that u want to add: ");
-                        book.addElement();
+
+                        System.out.println("Chose one of the options: \n" +
+                                "0.Top 10 books \n" +
+                                "1.Take book \n" +
+                                "2.Buy book");
+                        int temp = sc.nextInt();
+
+                        if(temp==1){
+                            System.out.println("Write number of the book that u want to take: ");
+                            int number = sc.nextInt();
+                            book.takeBook(number, user);
+                        }else if(temp==0){
+                            System.out.println("Here is top 10 books of our web - service: ");
+                            book.showRating();
+                        }
+
+                        //buyBook
+
+                    }else {
+                        System.out.println("Chose one of the options: \n" +
+                                "0.Top 10 books \n" +
+                                "1.Add free book \n" +
+                                "2.Add book with price ");
+                        int temp = sc.nextInt();
+
+                        if(temp==0){
+                            System.out.println("Here is top 10 books of our web - service: ");
+                            book.showRating();
+                        } else if (temp==1) {
+                            System.out.println("Add charachteristics of book that u want to add: ");
+                            book.addElement();
+                        }
+
+                        //Add book with price
                     }
-                }catch (Exception e){
+                }catch (Exception e) {
                     System.out.println("Book already exist" + e.getMessage());
                 }
+            }
 
-            }else if(choice.equals("l")) {
-                System.out.println("Enter your email: ");
-                String email = sc.nextLine();
-                System.out.println("Enter your password: ");
-                String password = sc.nextLine();
-                if(user.isValid(email,password,users)) {
-                    PreparedStatement stmt = null;
-                    ResultSet rs = null;
-                    try {
-                        Connection con = DriverManager.getConnection(connectionString,"postgres","keklolloh123");
-                        String statusQuery = "SELECT status FROM public.\"user\" WHERE email = ?";
-                        /* Statment */ stmt = con.prepareStatement(statusQuery);
-                        stmt.setString(1,email);
-                        /* Resultset */rs = stmt.executeQuery();
-                        if(rs.next()) {
-                            boolean userStatus = rs.getBoolean("status");
-                            if(userStatus) {
-                                System.out.println("Write number of the book that you want to take: ");
+
+
+
+
+            else if (choice == 0) {
+
+            System.out.println("Enter your email: ");
+            String email = sc.next();
+            System.out.println("Enter your password: ");
+            String password = sc.next();
+
+            if (user.isValid(email, password, users)) {
+
+                PreparedStatement stmt = null;
+                ResultSet rs = null;
+
+                try {
+
+                    Connection con = DriverManager.getConnection(connectionString, "postgres", "keklolloh123");
+                    String statusQuery = "SELECT status FROM public.\"user\" WHERE email = ?";
+                    stmt = con.prepareStatement(statusQuery);
+                    stmt.setString(1, email);
+                    rs = stmt.executeQuery();
+
+                    if (rs.next()) {
+
+                        boolean userStatus = rs.getBoolean("status");
+
+                        if (userStatus) {
+                            System.out.println("Chose one of the options: \n" +
+                                    "0.Top 10 books \n" +
+                                    "1.Take book \n" +
+                                    "2.Buy book");
+                            int temp = sc.nextInt();
+
+                            if(temp==1){
+                                System.out.println("Write number of the book that u want to take: ");
                                 int number = sc.nextInt();
-                                book.takeBook(number);
-                            }else if (!userStatus){
+                                book.takeBook(number, user);
+                            }else if(temp==0){
+                                book.showRating();
+                            }
+
+                        } else if (!userStatus) {
+                            System.out.println("Chose one of the options: \n" +
+                                    "0.Top 10 books \n" +
+                                    "1.Add free book \n" +
+                                    "2.Add book with price ");
+                            int temp = sc.nextInt();
+
+                            if(temp==0){
+                                System.out.println("Here is top 10 books of our web - service: ");
+                                book.showRating();
+                            } else if (temp==1) {
                                 System.out.println("Add charachteristics of book that u want to add: ");
                                 book.addElement();
                             }
+
+                            //AddBookWithPrice
+
                         }
-                    } catch(SQLException e) {
-                        throw new RuntimeException(e);
                     }
-                }
-            }
-
-        }catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-
-    }
-
-    public abstract void deleteElement(int id) throws SQLException;
-
-    public abstract void addElement() throws SQLException;
-}
-
-
-/*else{
-
-            System.out.print("Enter id of user that u want to delete: ");
-            int usersToDelete = sc.nextInt();
-            user.deleteElement(usersToDelete);
-
-            for(int i = 0; i< users.size(); i++){
-                if (users.get(i).getId()==usersToDelete){
-                    users.remove(i);}
-            }}
-        System.out.println("User was deleted successfully.");
-
-
-        package org.example;
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public abstract class Main {
-    public static void main(String[] args) throws SQLException {
-        String connectionString = "jdbc:postgresql://localhost:5432/library";
-        ArrayList<User> users = new ArrayList<>();
-        ArrayList<Book> books = new ArrayList<>();
-        Connection conn = null;
-        try{
-            Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection(connectionString,"postgres","keklolloh123");
-
-            String sql = "SELECT id, name, surname, status, email, password FROM public.\"user\" ORDER BY id";
-            Statement stat = conn.createStatement();
-
-            ResultSet rs = stat.executeQuery(sql);
-            while (rs.next()){
-                int    id = rs.getInt("id");
-                String name = rs.getString("name");
-                String surname = rs.getString("surname");
-                boolean status = rs.getBoolean("status");
-                String email = rs.getString("email");
-                String password = rs.getString("password");
-
-                User user = new User(id,name,surname,status,email,password);
-                users.add(user);
-            }
-
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }finally {
-            if (conn != null) {
-                try {
-                    conn.close();
                 } catch (SQLException e) {
-                    System.out.println("could not close the connection: " + e.getMessage());
+                    throw new RuntimeException(e);
                 }
             }
         }
 
-
-        try {
-            User user = new User(1,"1","1",true,"1","1");
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Do u want to Log in(l) or Sign up?(s):");
-            String choice = sc.nextLine();
-            if(choice.equals("s")){
-                user.addElement();
-                boolean userStatus = users.get(users.size() - 1).isStatus();
-                if (userStatus) {
-                    System.out.println("Write number of the book that u want to take: ");
-                    int number = sc.nextInt();
-                    Book book = new Book(1,"1",1,"1");
-                    book.takeBook(number);
-                }
-
-            }else if(choice.equals("l")){
-                System.out.println("Enter your email: ");
-                String email = sc.nextLine();
-                System.out.println("Enter your password: ");
-                String password = sc.nextLine();
-                if(user.isValid(email,password,users)){
-                    conn = DriverManager.getConnection(connectionString,"postgres","keklolloh123");
-                    String status = "SELECT status FROM public.\"user\" WHERE email = ?";
-                    PreparedStatement stmt = conn.prepareStatement(status);
-                    stmt.setString(1,email);
-                    ResultSet rs = stmt.executeQuery(status);
-                    boolean userStatus = rs.getBoolean("status");
-                    if(userStatus){
-                        System.out.println("Write number of the book that u want to take: ");
-                        int number = sc.nextInt();
-                        Book book = new Book(1,"1",1,"1");
-                        book.takeBook(number);
-                    }
-
-                }
-            }
-        }catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-
+    } catch (SQLException e) {
+        System.err.println(e.getMessage());
     }
+
+}
+
     public abstract void deleteElement(int id) throws SQLException;
 
     public abstract void addElement() throws SQLException;
 }
-
-
-/*else{
-
-            System.out.print("Enter id of user that u want to delete: ");
-            int usersToDelete = sc.nextInt();
-            user.deleteElement(usersToDelete);
-
-            for(int i = 0; i< users.size(); i++){
-                if (users.get(i).getId()==usersToDelete){
-                    users.remove(i);}
-            }}
-        System.out.println("User was deleted successfully.");*/
